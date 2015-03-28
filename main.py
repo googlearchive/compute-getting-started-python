3471a3f9403429805ae44e07c0fdd0172936a9f4
Jon Parrott <jonwayne@google.com>
2015-03-27 21:49:48 -0700

    Fixing indentation, license, long lines.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -27,138 +27,142 @@ from googleapiclient.discovery import build
 
 # [START list_instances]
 def list_instances(compute, project, zone):
-  result = compute.instances().list(project=project, zone=zone).execute()
-  return result['items']
+    result = compute.instances().list(project=project, zone=zone).execute()
+    return result['items']
 # [END list_instances]
 
 
 # [START create_instance]
 def create_instance(compute, project, zone, name):
-  sourceImage = "projects/debian-cloud/global/images/debian-7-wheezy-v20150320"
+    sourceImage = "projects/debian-cloud/global/images/debian-7-wheezy-v20150320"
 
-  config = {
-    'name': name,
-    'machineType': 'zones/%s/machineTypes/n1-standard-1' % zone,
+    config = {
+        'name': name,
+        'machineType': 'zones/%s/machineTypes/n1-standard-1' % zone,
 
-    # Specify the boot disk and the image to use as a source.
-    'disks': [
-      {
-        'boot': True,
-        'autoDelete': True,
-        'initializeParams': {
-          'sourceImage': sourceImage,
+        # Specify the boot disk and the image to use as a source.
+        'disks': [
+            {
+                'boot': True,
+                'autoDelete': True,
+                'initializeParams': {
+                    'sourceImage': sourceImage,
+                }
+            }
+        ],
+
+        # Specify a network interface with NAT to access the public
+        # internet.
+        'networkInterfaces': [{
+            'network': 'global/networks/default',
+            'accessConfigs': [
+                {'type': 'ONE_TO_ONE_NAT', 'name': 'External NAT'}
+            ]
+        }],
+
+        # Allow the instance to access cloud storage and logging.
+        'serviceAccounts': [{
+            'email': 'default',
+            'scopes': [
+                'https://www.googleapis.com/auth/devstorage.read_write',
+                'https://www.googleapis.com/auth/logging.write'
+            ]
+        }],
+
+        # Metadata is readable from the instance and allows you to
+        # pass configuration from deployment scripts to instances.
+        'metadata': {
+            'items': [{
+                # Startup script is automatically executed by the
+                # instance upon startup.
+                'key': 'startup-script',
+                'value': open('startup-script.sh', 'r').read()
+            }, {
+                'key': 'url',
+                'value': 'http://storage.googleapis.com/gce-demo-input/photo.jpg'
+            }, {
+                'key': 'text',
+                'value': 'Ready for dessert?'
+            }, {
+                # Every project has a default Cloud Storage bucket that's
+                # the same name as the project.
+                'key': 'bucket',
+                'value': project
+            }]
         }
-      }
-    ],
-
-    # Specify a network interface with NAT to access the public
-    # internet.
-    'networkInterfaces': [{
-      'network': 'global/networks/default',
-      'accessConfigs': [
-        {'type': 'ONE_TO_ONE_NAT', 'name': 'External NAT'}
-      ]
-    }],
-
-    # Allow the instance to access cloud storage and logging.
-    'serviceAccounts': [{
-      'email': 'default',
-      'scopes': [
-        'https://www.googleapis.com/auth/devstorage.read_write',
-        'https://www.googleapis.com/auth/logging.write'
-      ]
-    }],
-
-    # Metadata is readable from the instance and allows you to
-    # pass configuration from deployment scripts to instances.
-    'metadata': {
-      'items': [{
-        # Startup script is automatically executed by the
-        # instance upon startup.
-        'key': 'startup-script',
-        'value': open('startup-script.sh', 'r').read()
-      }, {
-        'key': 'url',
-        'value': 'http://storage.googleapis.com/gce-demo-input/photo.jpg'
-      }, {
-        'key': 'text',
-        'value': 'Ready for dessert?'
-      }, {
-        # Every project has a default Cloud Storage bucket that's
-        # the same name as the project.
-        'key': 'bucket',
-        'value': project
-      }]
     }
-  }
 
-  return compute.instances().insert(
-    project=project,
-    zone=zone,
-    body=config).execute()
+    return compute.instances().insert(
+        project=project,
+        zone=zone,
+        body=config).execute()
 # [END create_instance]
 
 
 # [START delete_instance]
 def delete_instance(compute, project, zone, name):
-  return compute.instances().delete(
-    project=project,
-    zone=zone,
-    instance=name).execute()
+    return compute.instances().delete(
+        project=project,
+        zone=zone,
+        instance=name).execute()
 # [END delete_instance]
 
 
 # [START wait_for_operation]
 def wait_for_operation(compute, project, zone, operation):
-  sys.stdout.write('Waiting for operation to finish')
-  while True:
-    result = compute.zoneOperations().get(project=project, zone=zone, operation=operation).execute()
-    if result['status'] == 'DONE':
-      print "done."
-      if 'error' in result:
-        raise Exception(result['error'])
-      return result
-    else:
-      sys.stdout.write('.')
-      sys.stdout.flush()
-      time.sleep(1)
+    sys.stdout.write('Waiting for operation to finish')
+    while True:
+        result = compute.zoneOperations().get(
+            project=project,
+            zone=zone,
+            operation=operation).execute()
+
+        if result['status'] == 'DONE':
+            print "done."
+            if 'error' in result:
+                raise Exception(result['error'])
+            return result
+        else:
+            sys.stdout.write('.')
+            sys.stdout.flush()
+            time.sleep(1)
 # [END wait_for_operation]
 
 
 # [START run]
 def run(project, zone, instance_name):
-  credentials = GoogleCredentials.get_application_default()
-  compute = build('compute', 'v1', credentials=credentials)
+    credentials = GoogleCredentials.get_application_default()
+    compute = build('compute', 'v1', credentials=credentials)
 
-  print 'Creating instance.'
+    print 'Creating instance.'
 
-  operation = create_instance(compute, project, zone, instance_name)
-  wait_for_operation(compute, project, zone, operation['name'])
+    operation = create_instance(compute, project, zone, instance_name)
+    wait_for_operation(compute, project, zone, operation['name'])
 
-  instances = list_instances(compute, project, zone)
+    instances = list_instances(compute, project, zone)
 
-  print 'Instances in project %s and zone %s:' % (project, zone)
-  for instance in instances:
-    print ' - ' + instance['name']
+    print 'Instances in project %s and zone %s:' % (project, zone)
+    for instance in instances:
+        print ' - ' + instance['name']
 
-  print 'Instance created. It will take a minute or two for the instance to complete work.'
-  print 'Check this URL: http://storage.googleapis.com/%s/output.png' % project
-  print 'Once the image is uploaded press enter to delete the instance.'
-  raw_input()
+    print 'Instance created. It will take a minute or two for the instance to complete work.'
+    print 'Check this URL: http://storage.googleapis.com/%s/output.png' % project
+    print 'Once the image is uploaded press enter to delete the instance.'
+    raw_input()
 
-  print 'Deleting instance.'
+    print 'Deleting instance.'
 
-  operation = delete_instance(compute, project, zone, instance_name)
-  wait_for_operation(compute, project, zone, operation['name'])
+    operation = delete_instance(compute, project, zone, instance_name)
+    wait_for_operation(compute, project, zone, operation['name'])
 # [END run]
 
 
 def main():
-  project = raw_input('What is your project ID? ')
-  zone = raw_input('What zone would you like to use? [us-central1-f] ') or 'us-central1-f'
-  instance_name = 'demo-instance'
-  run(project, zone, instance_name)
+    project = raw_input('What is your project ID? ')
+    zone = raw_input('What zone would you like to use? [us-central1-f] ') or 'us-central1-f'
+    instance_name = 'demo-instance'
+    run(project, zone, instance_name)
 
 
 if __name__ == '__main__':
-  main()
+    main()
